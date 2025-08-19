3eb77f339ec1318985323c0c8f032f2fc5ddde64
William Xavier dos Santos <xavier@gci.eb.mil.br>
2025-08-19 17:22:47 -0300

    mudança função sortearAmigo

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function sortearAmigo() {
 
         if ( de === para) {
             alert('Erro no sorteio. Por favor, insira mais um amigo.');
-            adicionarAmigo();
+            sortearAmigo;
             return;
         }
 
